a84d291f813fa19c6be24cde41eb41d4416c604a
EdwardCadena <earodriguez154@misena.edu.co>
2020-10-21 01:11:02 -0500

    Exercise number rouletta.

---
+++ NumberRoulette/NumberRoulette/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberRoulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int e, i, a;
+            for (e = 0; e < 10; e++)
+            {
+                for (i = 0; i < 10; i++) ;
+                Console.WriteLine("Ingrese un numero del 0 al 9");
+                a = Convert.ToInt32(Console.ReadLine());
+                a = (a + (i / 2)) % i;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
